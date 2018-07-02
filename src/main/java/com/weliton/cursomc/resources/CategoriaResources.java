96b6bd878ca4b8e106cf6f87eea988d605849a62
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-07-01 21:04:44 -0300

    inserindo uma nova Categoria cim POST

--- src/main/java/com/weliton/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/weliton/cursomc/resources/CategoriaResources.java
@@ -1,14 +1,15 @@
 package com.weliton.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.weliton.cursomc.domain.Categoria;
 import com.weliton.cursomc.services.CategoriaService;
@@ -25,17 +26,13 @@ public class CategoriaResources {
 	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
-		/* Categoria cat1 = new Categoria(1, "informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
 
-		
-		List<Categoria>lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-*/
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
